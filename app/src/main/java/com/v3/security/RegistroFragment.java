941b2cf8f5d6aa9df2767621c15c76f19b2833f7
Skylake <gaston.92.08@gmail.com>
2018-04-02 04:32:11 -0300

    Bueno

--- app/src/main/java/com/v3/security/RegistroFragment.java
+++ app/src/main/java/com/v3/security/RegistroFragment.java
@@ -1,12 +1,43 @@
 package com.v3.security;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
+import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.v3.security.Util.VolleySingleton;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.app.Activity.RESULT_OK;
 
 
 /**
@@ -22,7 +53,18 @@ public class RegistroFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+    EditText etNombre,etApellido,etMotivo;
+    ImageView ivFotoRegistro;
+    Button btnFotoRegistro, btnEnviarRegistro;
+    Context context;
+    Bitmap bitmap;
+    String path;
+    private final String CARPETA_RAIZ = "misImagenesPueba/";
+    private final String RUTA_IMAGEN = CARPETA_RAIZ + "misFotos";
+    final int CODIGO_FOTO = 30;
+    final int CODIGO_SELECCIONA = 40;
+    JsonObjectRequest jsonObjectRequest;
+    StringRequest stringRequest;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -64,7 +106,29 @@ public class RegistroFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registro, container, false);
+        View view = inflater.inflate(R.layout.fragment_registro, container, false);
+        context = this.getContext();
+        etNombre= view.findViewById(R.id.etNombre);
+        etApellido= view.findViewById(R.id.etApellido);
+        etMotivo= view.findViewById(R.id.etMotivo);
+        btnEnviarRegistro= view.findViewById(R.id.btnEnviarRegistro);
+        btnFotoRegistro= view.findViewById(R.id.btnFotoRegistro);
+        ivFotoRegistro = view.findViewById(R.id.ivFotoRegistro);
+
+        btnFotoRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tomarFoto();
+            }
+        });
+        btnEnviarRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ingresarVisitas();
+            }
+        });
+        return view;
+
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -89,17 +153,151 @@ public class RegistroFragment extends Fragment {
         super.onDetach();
         mListener = null;
     }
+    private void ingresarVisitas(){
+        String url = "http://192.168.0.14/seguridad/registrarVisita.php?";
+        stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                String idIngresos = "a";
+                String idGuardia = "1";
+                String nombre = "Marcelo";
+                String apellido = "Martinez";
+                String motivo = "Nada";
+                String foto = convertirImgString(bitmap);
+                Map<String,String>para = new HashMap<>();
+                para.put("idIngresos",idIngresos);
+                para.put("idGuardia",idGuardia);
+                para.put("nombre",nombre);
+                para.put("apellido",apellido);
+                para.put("motivo",motivo);
+                para.put("foto",foto);
+                return para;
+            }
+        };
+        VolleySingleton.getInstanciaVolley(getContext()).addToRequestQueue(stringRequest);
+
+    }
+    private String convertirImgString(Bitmap bitmap) {
+        ByteArrayOutputStream array = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, array);
+        byte[] imagenByte = array.toByteArray();
+        String imagenString = Base64.encodeToString(imagenByte, Base64.DEFAULT);
+        return imagenString;
+    }
+    private void tomarFoto() {
+        File fileImagen = new File(Environment.getExternalStorageDirectory(), RUTA_IMAGEN);
+        boolean isCreada = fileImagen.exists();
+        String nombreImagen = "";
+        if (isCreada == false) {
+            isCreada = fileImagen.mkdirs();
+        }
+
+        if (isCreada == true) {
+            nombreImagen = (System.currentTimeMillis() / 1000) + ".jpg";
+        }
+
+
+        path = Environment.getExternalStorageDirectory() +
+                File.separator + RUTA_IMAGEN + File.separator + nombreImagen;
+
+        File imagen = new File(path);
+
+        Intent intent = null;
+        intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        ////
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            String authorities = getContext().getPackageName() + ".provider";
+            Uri imageUri = FileProvider.getUriForFile(context, authorities, imagen);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        } else {
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(imagen));
+        }
+        startActivityForResult(intent, CODIGO_FOTO);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case CODIGO_SELECCIONA:
+                    Uri miPath = data.getData();
+                    ivFotoRegistro.setImageURI(miPath);
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(context.getContentResolver(), miPath);
+                        ivFotoRegistro.setImageBitmap(bitmap);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case CODIGO_FOTO:
+                    MediaScannerConnection.scanFile(context, new String[]{path}, null, new MediaScannerConnection.OnScanCompletedListener() {
+                        @Override
+                        public void onScanCompleted(String s, Uri uri) {
+                            Log.i("Ruta de almacenamiento", "Path: " + path);
+                        }
+                    });
+                    bitmap = BitmapFactory.decodeFile(path);
+                    bitmap = redimensionarImagen(bitmap, 1280, 960);
+
+                    rotateimagen(bitmap);
+                    // imageView.setImageBitmap(bitmap);
+
+                    break;
+            }
+        }
+    }
+
+    private Bitmap redimensionarImagen(Bitmap bitmap, float anchoNuevo, float altoNuevo) {
+        int ancho = bitmap.getWidth();
+        int alto = bitmap.getHeight();
+
+        if (ancho > anchoNuevo || alto > altoNuevo) {
+            float escalaAncho = anchoNuevo / ancho;
+            float escalaAlto = altoNuevo / alto;
+
+            Matrix matrix = new Matrix();
+            matrix.postScale(escalaAncho, escalaAlto);
+
+            return Bitmap.createBitmap(bitmap, 0, 0, ancho, alto, matrix, false);
+
+        } else {
+            return bitmap;
+        }
+
+    }
+    private void rotateimagen (Bitmap bitmap){
+        ExifInterface exifInterface = null;
+        try {
+            exifInterface=new ExifInterface(path);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        int orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION,ExifInterface.ORIENTATION_UNDEFINED);
+        Matrix matrix = new Matrix();
+        switch (orientation){
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                matrix.setRotate(90);
+
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                matrix.setRotate(180);
+                break;
+            default:
+        }
+        Bitmap rotateBitmap = Bitmap.createBitmap(bitmap,0,0,bitmap.getWidth(),bitmap.getHeight(),matrix,true);
+        ivFotoRegistro.setImageBitmap(rotateBitmap);
+    }
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
